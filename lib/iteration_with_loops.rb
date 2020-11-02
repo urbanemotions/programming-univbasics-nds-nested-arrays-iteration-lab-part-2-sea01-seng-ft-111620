858a2a3055ed236f1f7234c39fa3f8b8b829c3fe
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 02:36:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,12 +7,14 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0 
     lowest_temperature_element = ""
-    while element_index < src[element_index].count do
+    while element_index < src[row_index].count do
       if src[element_index] < lowest_temperature_element
         lowest_temperature_element = src[element_index] 
       end
       element_index += 1 
     end
+    daily_lowest_temperature << lowest_temperature_element
     row_index += 1 
   end
+  daily_lowest_temperature
 end
